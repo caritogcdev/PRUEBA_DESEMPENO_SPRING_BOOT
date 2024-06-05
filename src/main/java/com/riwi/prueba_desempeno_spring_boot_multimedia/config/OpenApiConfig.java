ac90ab36db04ee047dc3f3a897e54d515c5c5d70
caritogcdev <carito940905@gmail.com>
2024-06-05 12:40:49 -0500

    General Swagger config dependence

--- src/main/java/com/riwi/prueba_desempeno_spring_boot_multimedia/config/OpenApiConfig.java
+++ src/main/java/com/riwi/prueba_desempeno_spring_boot_multimedia/config/OpenApiConfig.java
@@ -1,4 +1,14 @@
 package com.riwi.prueba_desempeno_spring_boot_multimedia.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Configuración del Swagger
+ */
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "Api para la gestión de contenido multimedia de las clases impartidas en Riwi", version = "1,0", description = "Documentación API de destión de contenido multimedia"))
 public class OpenApiConfig {
+
 }
