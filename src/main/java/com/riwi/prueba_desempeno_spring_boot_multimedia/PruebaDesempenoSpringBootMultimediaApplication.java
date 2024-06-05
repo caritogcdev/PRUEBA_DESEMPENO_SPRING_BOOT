2dff3ec24644502c156e8de217f517fe3fbe7782
caritogcdev <carito940905@gmail.com>
2024-06-05 06:37:57 -0500

    First commit

---
+++ src/main/java/com/riwi/prueba_desempeno_spring_boot_multimedia/PruebaDesempenoSpringBootMultimediaApplication.java
@@ -0,0 +1,13 @@
+package com.riwi.prueba_desempeno_spring_boot_multimedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaDesempenoSpringBootMultimediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaDesempenoSpringBootMultimediaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/riwi/prueba_desempeno_spring_boot_multimedia/PruebaDesempenoSpringBootMultimediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riwi.prueba_desempeno_spring_boot_multimedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaDesempenoSpringBootMultimediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
